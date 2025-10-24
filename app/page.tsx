333a74e3715447be723bb02f00c1213b42c6b6a5
YONATANEMEKETE <yonatanemekete33@gmail.com>
2025-10-23 21:57:21 -0700

    setup color, font....

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      REPUTE
+    <div className="flex min-h-screen items-center justify-center bg-secondary font-main dark:bg-black">
+      <h1 className="text-4xl font-bold text-myaccent">REPUTE</h1>
     </div>
   );
 }
